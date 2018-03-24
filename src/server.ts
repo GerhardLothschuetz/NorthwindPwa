7aeb4df310bda274e25988c5143f0f4e6bb5fe4b
Surf <lothy@gmx.de>
2018-03-24 01:42:42 +0100

    MY First Big Ass Commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import * as SqlLite from 'sqlite3';
+
+const db = new SqlLite.Database('D:/Git/NodeDemo/db/Northwind/Northwind.sl3');
+db.serialize(() =>{
+    //db.run("CREATE TABLE lorem (info TEXT)");
+    /*var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (var i = 0; i < 10; i++) {
+        stmt.run("Ipsum1 " + i);
+    }
+    stmt.finalize();*/
+        
+    db.each("SELECT * FROM Customers", (err, row) => {
+             console.log(row.CustomerID + ": " + row.CompanyName);
+             //console.log(row.CompanyName);
+    
+    
+    })
+    
+})
+
+
+db.close();
